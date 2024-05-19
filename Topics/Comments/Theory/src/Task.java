c604a636570ba349ff51cb9cd99ab7a93a04cf7c
firjam <firdaus@tandemasia.com>
2024-05-19 22:09:28 +0800

    Initial commit

---
+++ Topics/Comments/Theory/src/Task.java
@@ -0,0 +1,12 @@
+// You can experiment here, it won’t be checked
+
+public class Task {
+  public static void main(String[] args) {
+    // put your code here
+    System.out.print("Hello, ");
+    //System.out.print("world");
+    //System.out.print(" and ");
+    System.out.print("Java");
+    System.out.print(" platform");
+  }
+}
